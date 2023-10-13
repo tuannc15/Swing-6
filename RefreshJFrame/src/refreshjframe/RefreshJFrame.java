f4265d031b4f5f84c57f0915ea47fe35726869f3
nctuan2013 <54241848+nctuan2013@users.noreply.github.com>
2023-10-13 21:13:10 +0700

    Buoi7

---
+++ RefreshJFrame/src/refreshjframe/RefreshJFrame.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package refreshjframe;
+
+/**
+ *
+ * @author nctuan
+ */
+public class RefreshJFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Show show = new Show();
+        show.setVisible(true);
+    }
+
+
+}
